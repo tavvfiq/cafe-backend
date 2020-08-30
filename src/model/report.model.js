f93ec1065313780040da2d792807dd51d88ae578
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-08-30 22:52:49 +0700

    update report

--- src/model/report.model.js
+++ src/model/report.model.js
@@ -2,83 +2,81 @@ const database = require("../config/config");
 
 const historyQuery = `SELECT history.invoice, history.cashier, history.order_date, menu.name AS menu_name, menu_order_history.quantity AS menu_quantity, history.amount AS total_amount FROM history JOIN menu_order_history ON menu_order_history.invoice = history.invoice JOIN menu ON menu_order_history.menu_id = menu.id`;
 const historyModel = {
-    alignHelper: function (data) {
-        let arrayOfOrder = [
-            []
-        ];
-        let objRes = [];
-        let lastElement = {};
-        let orderIdx = 0;
-        let dataLength = data.length;
-        for (let i = 0; i < dataLength; i++) {
-            if (i === 0) {
-                arrayOfOrder[orderIdx].push({
-                    menu_name: data[i].menu_name,
-                    menu_quantity: data[i].menu_quantity
-                });
-                lastElement = data[i];
-            } else if (data[i].invoice !== lastElement.invoice) {
-                objRes[orderIdx] = [];
-                objRes[orderIdx].push({
-                    invoice: lastElement.invoice,
-                    cashier: lastElement.cashier,
-                    order_date: lastElement.order_date,
-                    menu_order: arrayOfOrder[orderIdx],
-                    total_amount: lastElement.total_amount
-                });
-                orderIdx++;
-                arrayOfOrder[orderIdx] = [];
-                arrayOfOrder[orderIdx].push({
-                    menu_name: data[i].menu_name,
-                    menu_quantity: data[i].menu_quantity
-                });
-                lastElement = data[i];
-            } else {
-                arrayOfOrder[orderIdx].push({
-                    menu_name: data[i].menu_name,
-                    menu_quantity: data[i].menu_quantity
-                });
-                lastElement = data[i];
-            }
-            if (i === dataLength - 1) {
-                objRes[orderIdx] = [];
-                objRes[orderIdx].push({
-                    invoice: lastElement.invoice,
-                    cashier: lastElement.cashier,
-                    order_date: lastElement.order_date,
-                    menu_order: arrayOfOrder[orderIdx],
-                    total_amount: lastElement.total_amount
-                });
-            }
-        }
-        return objRes;
-    },
-    showAllHistory: function () {
-        return new Promise((resolve, reject) => {
-            const showAllQuery = historyQuery;
-            database.query(showAllQuery, (err, data) => {
-                if (!err) {
-                    const objRes = this.alignHelper(data);
-                    resolve(objRes);
-                } else {
-                    reject(err);
-                }
-            });
+  alignHelper: function (data) {
+    let arrayOfOrder = [[]];
+    let objRes = [];
+    let lastElement = {};
+    let orderIdx = 0;
+    let dataLength = data.length;
+    for (let i = 0; i < dataLength; i++) {
+      if (i === 0) {
+        arrayOfOrder[orderIdx].push({
+          menu_name: data[i].menu_name,
+          menu_quantity: data[i].menu_quantity,
         });
-    },
-    showHistoryByInvoice: function (invoice) {
-        return new Promise((resolve, reject) => {
-            const queryByInvoice = `${historyQuery} WHERE history.invoice = ${invoice}`;
-            database.query(queryByInvoice, (err, data) => {
-                if (!err) {
-                    const objRes = this.alignHelper(data);
-                    resolve(objRes);
-                } else {
-                    reject(err);
-                }
-            });
+        lastElement = data[i];
+      } else if (data[i].invoice !== lastElement.invoice) {
+        objRes[orderIdx] = [];
+        objRes[orderIdx] = {
+          invoice: lastElement.invoice,
+          cashier: lastElement.cashier,
+          order_date: lastElement.order_date,
+          menu_order: arrayOfOrder[orderIdx],
+          total_amount: lastElement.total_amount,
+        };
+        orderIdx++;
+        arrayOfOrder[orderIdx] = [];
+        arrayOfOrder[orderIdx].push({
+          menu_name: data[i].menu_name,
+          menu_quantity: data[i].menu_quantity,
         });
+        lastElement = data[i];
+      } else {
+        arrayOfOrder[orderIdx].push({
+          menu_name: data[i].menu_name,
+          menu_quantity: data[i].menu_quantity,
+        });
+        lastElement = data[i];
+      }
+      if (i === dataLength - 1) {
+        objRes[orderIdx] = {};
+        objRes[orderIdx] = {
+          invoice: lastElement.invoice,
+          cashier: lastElement.cashier,
+          order_date: lastElement.order_date,
+          menu_order: arrayOfOrder[orderIdx],
+          total_amount: lastElement.total_amount,
+        };
+      }
     }
-}
+    return objRes;
+  },
+  showAllHistory: function () {
+    return new Promise((resolve, reject) => {
+      const showAllQuery = historyQuery;
+      database.query(showAllQuery, (err, data) => {
+        if (!err) {
+          const objRes = this.alignHelper(data);
+          resolve(objRes);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  },
+  showHistoryByInvoice: function (invoice) {
+    return new Promise((resolve, reject) => {
+      const queryByInvoice = `${historyQuery} WHERE history.invoice = ${invoice}`;
+      database.query(queryByInvoice, (err, data) => {
+        if (!err) {
+          const objRes = this.alignHelper(data);
+          resolve(objRes);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  },
+};
 
 module.exports = historyModel;
